55a6f819dc28a9ce7d792afe4c62b313275bde05
vosfows <73765410+vosfows@users.noreply.github.com>
2020-12-06 12:14:08 +0800

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,39 @@
+
+<?php
+    
+	$user = $_POST['user'];
+    $pass = $_POST['pass'];
+ 
+    // Data Connect
+
+    $conn = new mysqli("localhost","root","","website");
+    echo "string";
+    if($conn->connect_error){
+        die("Failed to connect : ".$conn->connect_error);
+    } else{
+
+       
+    	echo "pasok";
+        $stmt = $conn->prepare("select * from pinuno where user = ?");
+        $stmt->bind_param("s", $user);
+        $stmt->execute();
+        $stmt_result = $stmt->get_result();
+
+        if($stmt_result->num_rows > 0) {
+        	echo "pasok uli";
+            $data = $stmt_result->fetch_assoc();
+
+            if($data['pass'] === $pass) {
+            	echo "pumasok na";
+                header('Location: authorize.html');
+            }
+        } else {
+            
+        }
+
+    }
+?>
+
+
+
+
